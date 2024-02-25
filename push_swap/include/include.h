764f1581cef845ea423fe333c7f2e999a43844bf
d3str3k <d3str3k@samsung-led.home>
2024-02-25 10:41:53 +0100

    Estructuración por carpetas

---
+++ push_swap/include/include.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxgarci <maxgarci@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 10:31:09 by maxgarci          #+#    #+#             */
+/*   Updated: 2024/02/25 10:35:17 by maxgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_H
+# define INCLUDE_H
+
+# include "../src/stack/stack.h"
+# include "ft_printf/ft_printf.h"
+
+#endif
