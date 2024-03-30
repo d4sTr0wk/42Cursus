b0de34798ea2c63967c8e512c3904cae439b8b10
Máximo García <63814427+d4sTr0wk@users.noreply.github.com>
2024-03-30 23:37:41 +0100

    minitalk comienzo

---
+++ minitalk/libft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxgarci <maxgarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 19:28:09 by maxgarci          #+#    #+#             */
+/*   Updated: 2024/03/30 23:35:59 by maxgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putchar_fd(char c, int fd); 
+
+#endif
