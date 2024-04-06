b1228a712ef8f291916e83cfc539fcb79880bdb7
d3str3k <d3str3k@samsung-led.home>
2024-04-06 16:43:24 +0200

    hola

---
+++ minitalk/bonus/client_bonus.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxgarci <maxgarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/30 23:19:20 by maxgarci          #+#    #+#             */
+/*   Updated: 2024/04/01 23:56:52 by maxgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../src/minitalk.h"
+
+void    sendSignal(int pid, unsigned char character)
+{
+    int i;
+    int err;
+
+    i = CHAR_BIT;
+    
+    while (i-- > 0)
+    {
+        if ((character >> i ) & 1)
+            err = kill(pid, SIGUSR1);
+        else
+            err = kill(pid, SIGUSR2);
+        if (err == -1)
+        {
+            ft_putstr_fd("Error: sendSignal", 2);
+            return ;
+        }
+        usleep(42);
+    }
+}
+
+void    sendMessage(int pid, char *message)
+{
+    int i;
+
+    i = -1;
+    while (message[++i] != '\0')
+        sendSignal(pid, message[i]);
+    if (kill(pid, '\0'))
+    {
+        ft_putstr_fd("Error: sendMessage", 2);
+        return ;
+    }
+}
+
+int	main(int argc, char **argv)
+{
+    char    *message;
+    int     pid;
+
+    if (argc != 3)
+    {
+        ft_putstr_fd("Error with arguments\n", 2);
+        return (1);
+    }
+    pid = ft_atoi(argv[1]);
+    message = argv[2];
+    sendMessage(pid, message);
+	return (0);
+}
---
+++ minitalk/bonus/server_bonus.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxgarci <maxgarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/30 23:21:08 by maxgarci          #+#    #+#             */
+/*   Updated: 2024/04/01 23:58:23 by maxgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	sig_handler(int signum)
+{
+	static unsigned char	character = 0;
+	static int				bit_index = 0;
+
+	character |= (signum == SIGUSR1);
+	bit_index++;
+	if (bit_index == CHAR_BIT)
+	{
+		if (character == '\0')
+			ft_putchar_fd('\n', 1);
+		else
+			ft_putchar_fd(character, 1);
+		character = 0;
+		bit_index = 0;
+	}
+	else
+		character <<= 1;
+}
+
+int	main(void)
+{
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
+	
+	int	pid;
+
+	pid = getpid();
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(pid, 1);
+	ft_putchar_fd('\n', 1);
+
+	while (1)
+		pause();
+	return (0);
+}
