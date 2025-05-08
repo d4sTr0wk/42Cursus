d286e28c36143c00d6bade50239fa0cfeb3eb825
Máximo G <maxgarci@localhost.localdomain>
2025-05-08 08:12:10 +0200

    philo: go_to_think included

--- philo/src/philo_utils.c
+++ philo/src/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: maxgarci <maxgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 10:27:25 by maxgarci          #+#    #+#             */
-/*   Updated: 2025/05/07 11:10:54 by maxgarci         ###   ########.fr       */
+/*   Updated: 2025/05/08 12:01:59 by maxgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,29 +25,35 @@ int	am_i_dead(struct timeval *now, struct timeval *last_time,
 	return (NO);
 }
 
-int	check_forks_freed(t_philo_data *data, struct timeval *init_time,
-	struct timeval *now)
+static void	take_forks(t_philo_data *data, struct timeval *now)
+{
+	data->forks_taken[data->left_fork] = YES;
+	data->forks_taken[data->right_fork] = YES;
+	pthread_mutex_unlock(&data->forks_mutexes[data->left_fork]);
+	pthread_mutex_unlock(&data->forks_mutexes[data->right_fork]);
+	pthread_mutex_lock(data->echo_mutex);
+	gettimeofday(now, NULL);
+	printf(RESET "%ld %d has taken fork %d\n",
+		(long)(((now->tv_sec * 1000) + (now->tv_usec / 1e3))
+			- ((data->init_time->tv_sec * 1000)
+				+ (data->init_time->tv_usec / 1e3))),
+		data->id, data->left_fork);
+	printf(RESET "%ld %d has taken fork %d\n",
+		(long)(((now->tv_sec * 1000) + (now->tv_usec / 1e3))
+			- ((data->init_time->tv_sec * 1000)
+				+ (data->init_time->tv_usec / 1e3))),
+		data->id, data->right_fork);
+	pthread_mutex_unlock(data->echo_mutex);
+}
+
+int	check_forks_freed(t_philo_data *data, struct timeval *now)
 {
 	pthread_mutex_lock(&data->forks_mutexes[data->left_fork]);
 	pthread_mutex_lock(&data->forks_mutexes[data->right_fork]);
 	if (data->forks_taken[data->left_fork] == NO
 		&& data->forks_taken[data->right_fork] == NO)
 	{
-		data->forks_taken[data->left_fork] = YES;
-		data->forks_taken[data->right_fork] = YES;
-		pthread_mutex_unlock(&data->forks_mutexes[data->left_fork]);
-		pthread_mutex_unlock(&data->forks_mutexes[data->right_fork]);
-		pthread_mutex_lock(data->echo_mutex);
-		gettimeofday(now, NULL);
-		printf(RESET "%ld %d has taken fork %d\n",
-			(long)(((now->tv_sec * 1000) + (now->tv_usec / 1e3))
-				- ((init_time->tv_sec * 1000) + (init_time->tv_usec / 1e3))),
-			data->id, data->left_fork);
-		printf(RESET "%ld %d has taken fork %d\n",
-			(long)(((now->tv_sec * 1000) + (now->tv_usec / 1e3))
-				- ((init_time->tv_sec * 1000) + (init_time->tv_usec / 1e3))),
-			data->id, data->right_fork);
-		pthread_mutex_unlock(data->echo_mutex);
+		take_forks(data, now);
 		return (FN_SUCESSED);
 	}
 	pthread_mutex_unlock(&data->forks_mutexes[data->left_fork]);
@@ -72,14 +78,14 @@ void	set_simulation_active(t_args *args, int value)
 	pthread_mutex_unlock(&args->simulation_mutex);
 }
 
-void	kill_philosopher(t_philo_data *data, struct timeval *now,
-	struct timeval *init_time)
+void	kill_philosopher(t_philo_data *data, struct timeval *now)
 {
 	pthread_mutex_lock(data->echo_mutex);
-	printf(RED "%ld %d died\n",
-		(long)(((now->tv_sec - init_time->tv_sec) * 1000)
-			+ (long)((now->tv_usec - init_time->tv_usec) / 1e3)),
-		data->id);
+	if (is_simulation_active(data->args))
+		printf(RED "%ld %d died\n",
+			(long)(((now->tv_sec - data->init_time->tv_sec) * 1000)
+				+ (long)((now->tv_usec - data->init_time->tv_usec) / 1e3)),
+			data->id);
 	set_simulation_active(data->args, 0);
 	pthread_mutex_unlock(data->echo_mutex);
 	free(data);
