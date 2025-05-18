00e8f5d5a24899b984378f5cf8bca465d599b650
Maximo Garcia Aroca <maxgarci@c2r10s6.42malaga.com>
2025-05-18 10:46:54 +0200

    philo: segfault en add_my_turn arreglado

--- philo/src/check_forks_freed.c
+++ philo/src/check_forks_freed.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*																			*/
 /*														:::	  ::::::::   */
-/*   check_forks_freed.c								:+:	  :+:	:+:   */
+/*   check_forks_freed.c                                :+:      :+:    :+:   */
 /*													+:+ +:+		 +:+	 */
 /*   By: maxgarci <maxgarci@student.42.fr>		  +#+  +:+	   +#+		*/
 /*												+#+#+#+#+#+   +#+		   */
 /*   Created: 2025/05/08 15:33:18 by maxgarci		  #+#	#+#			 */
-/*   Updated: 2025/05/12 09:55:48 by maxgarci		 ###   ########.fr	   */
+/*   Updated: 2025/05/18 10:24:18 by maxgarci         ###   ########.fr       */
 /*																			*/
 /* ************************************************************************** */
 
@@ -51,22 +51,26 @@ static void	print_queue(t_philo_data *data)
 	pthread_mutex_unlock(data->echo_mutex);
 }
 
-static void	add_my_turn(t_philo_data *data, struct timeval *now)
+static void	add_my_turn(t_philo_data *data)
 {
 	t_queue_node	*tmp;
 
-	if (data->in_queue == YES)
+	pthread_mutex_lock(&data->args->fqueue->queue_mutex);
+	if (data->in_queue == YES || data->my_ticket != NULL)
+	{
+		pthread_mutex_unlock(&data->args->fqueue->queue_mutex);
 		return;
+	}
 	data->my_ticket = (t_queue_node *)malloc(sizeof(t_queue_node));
 	if (data->my_ticket == NULL)
 	{
 		ft_putstr_fd("Allocating my ticket node error", 2);
+		pthread_mutex_unlock(&data->args->fqueue->queue_mutex);
 		return ;
 	}
 	data->my_ticket->id = data->id;
 	data->my_ticket->next = NULL;
 	data->my_ticket->prev = NULL;
-	pthread_mutex_lock(&data->args->fqueue->queue_mutex);
 	tmp = data->args->fqueue->front;
 	if (tmp == NULL)
 		data->args->fqueue->front = data->my_ticket;
@@ -79,7 +83,6 @@ static void	add_my_turn(t_philo_data *data, struct timeval *now)
 		tmp->next = data->my_ticket;
 		data->my_ticket->prev = tmp;
 	}
-	tmp = data->args->fqueue->front;
 	data->in_queue = YES;
 	pthread_mutex_unlock(&data->args->fqueue->queue_mutex);
 }
@@ -111,18 +114,18 @@ static void	delete_node(t_philo_data *data)
 	pthread_mutex_unlock(&data->args->fqueue->queue_mutex);
 }
 
-int	my_turn(t_philo_data *data, struct timeval *now)
+int	my_turn(t_philo_data *data)
 {
 	t_queue_node	*tmp;
 
+	pthread_mutex_lock(&data->args->fqueue->queue_mutex);
 	if (data->in_queue == NO)
-		add_my_turn(data, now);
+		add_my_turn(data);
 	if (data->first_round == YES)
 	{
 		data->first_round = NO;
 		return (YES);
 	}
-	pthread_mutex_lock(&data->args->fqueue->queue_mutex);
 	tmp = data->args->fqueue->front;
 	while (tmp != NULL)
 	{
@@ -146,7 +149,7 @@ int	my_turn(t_philo_data *data, struct timeval *now)
 
 int	check_forks_freed(t_philo_data *data, struct timeval *now)
 {
-	if (my_turn(data, now) == NO)
+	if (my_turn(data) == NO)
 		return (FN_FAILED);
 	pthread_mutex_lock(&data->forks_mutexes[data->left_fork]);
 	pthread_mutex_lock(&data->forks_mutexes[data->right_fork]);
